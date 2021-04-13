56f2bd1bd5fcf913ea84361a067fa066fcf3f0ea
cmyerscda <72218687+cmyerscda@users.noreply.github.com>
2021-04-13 15:34:45 -0400

    Update server_disable.py

--- server_disable.py
+++ server_disable.py
@@ -2,6 +2,7 @@ import json
 import requests, sys, base64, collections
 import urllib3
 import getpass
+import string
 
 #Disable SSL Warnings if cert is untrusted
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -21,6 +22,8 @@ except Exception as error:
 SERVER_IP=sys.argv[4]
 
 SERVER_STATE=sys.argv[5]
+JIRA_USERNAME=sys.argv[6]
+JIRA_PASS=sys.argv[7]
 
 headers={
 "X-NITRO-USER":NITRO_USER,
@@ -57,3 +60,24 @@ for l in server_data['server']:
             }
             change_state = requests.post("https://%s/nitro/v1/config/server?action=enable"%(NITRO_SERVER),headers=headers,data=json.dumps(payload),verify=False)
 
+
+            
+            
+            #----------------------------------------------------------------
+jira_url = 'https://criticaldesign.atlassian.net/rest/servicedeskapi/request'
+headers = {"Content-Type": "application/json"}
+#jirasm_payload = '{"serviceDeskId": "1", "requestTypeId": "16", "requestFieldValues": {"summary": "Developer Setup Load Balanced VIP via REST", "description": "These are the actions taken"}}'
+jirasm_payload = '{"serviceDeskId": "1", "requestTypeId": "16", "requestFieldValues": {"summary": "Developer Setup Load Balanced VIP via REST", "description": "STATUS"}}'
+MyString = "The following IP was " + SERVER_STATE + " : " + SERVER_IP
+jirasm_payload = jirasm_payload.replace('STATUS', MyString)
+r = requests.post(jira_url, auth=(JIRA_USERNAME, JIRA_PASS), data=jirasm_payload, headers=headers)
+
+print(r.status_code)
+#print(r.text)
+jira_text = r.content
+jira_data = json.loads(jira_text)
+for link in jira_data['_links'] :
+    agent_link = jira_data['_links']['agent']
+    print("Jira Service Management Ticket #: " + agent_link)
+    #print(agent_link)
+    exit()
