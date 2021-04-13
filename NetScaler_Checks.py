cff2964242aeae7841000a80db7ec5c45882aab3
cmyerscda <72218687+cmyerscda@users.noreply.github.com>
2021-04-13 16:46:06 -0400

    Update NetScaler_Checks.py

--- NetScaler_Checks.py
+++ NetScaler_Checks.py
@@ -185,7 +185,7 @@ for link in jira_data['_links'] :
     #-------------------------------------------------------------------------
 tic = agent_link
 tic = tic.replace('https://criticaldesign.atlassian.net/browse/','')
-url = 'https://criticaldesign.atlassian.net/rest/api/2/issue/' + tic + '/attachments'
+url = 'https://criticaldesign.atlassian.net/rest/api/3/issue/' + tic + '/attachments'
 #url = 'https://criticaldesign.atlassian.net/rest/api/3/issue/10100/attachments'
 #url = 'https://criticaldesign.atlassian.net/rest/api/2/attachments/10100'
 t = requests.post(url,auth=(JIRA_USERNAME, JIRA_PASS), files=files, headers=aheaders)
