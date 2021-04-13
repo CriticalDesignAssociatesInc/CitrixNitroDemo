03258298ab457cc1da0af1f2ffc2a423e81a374c
cmyerscda <72218687+cmyerscda@users.noreply.github.com>
2021-04-13 13:44:54 -0400

    Update NetScaler_Checks.py

--- NetScaler_Checks.py
+++ NetScaler_Checks.py
@@ -4,7 +4,7 @@ import urllib3
 import csv
 import getpass
 import os
-#import pandas as pd
+import pandas as pd
 
 #Get a list of LB vServers, Service Group Bindings, Server Group members, 
 #Service Group Member Ports, Service Group Monitors, Service Group Monitor HTTP Requests, LB vServer Status, Backend Server Status
@@ -146,5 +146,5 @@ with open('monitor_status.csv','w', newline='') as f:
             backend = 'N/A'
             writer.writerow([lbvs_name,svg_grpname,svrname,port,monname,httpreq,lbvs_stat,svrstate,backend])
 
-"""df = pd.read_csv('monitor_status.csv')
-print(df)"""
+df = pd.read_csv('monitor_status.csv')
+print(df)
